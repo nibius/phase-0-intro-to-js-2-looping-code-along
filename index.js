48008859bee56b758deeecfa72ccd96093e3dcf5
Giovanni <nibius9@gmail.com>
2021-09-19 22:30:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const array = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(array ){
+    for(let i =0; i< array .length; i++){
+        console.log('Thank you, '+ array [i] + ', for the wonderful gift');
+
+ }
+ return array ;
+
+}
+
+writeCards(array);
+
+
+
+var num = 10;
+
+function countDown(){
+	while (num >0){
+		console.log(num);
+		num--;
+	}
+
+}
